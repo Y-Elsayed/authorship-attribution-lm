1193f11f4e77c24ded2768b70d3a67e09b9676f2
Y-Elsayed <yelsayed003@gmail.com>
2025-03-04 00:23:23 -0600

    Added prediction and devset evaluation functions

--- hf_sequence_classifier.py
+++ hf_sequence_classifier.py
@@ -3,13 +3,14 @@ import torch
 from authors_dataset import AuthorsDataset
 
 class SequenceClassifier:
-    def __init__(self, num_labels, model_name="bert-base-uncased"):
+    def __init__(self, num_labels, model_name="bert-base-uncased",max_length=512):
         self.num_labels = num_labels
         self.model_name = model_name
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.max_length = max_length
 
-    def train(self, authors_data, epochs=3,batch_size=16, max_length=512):
+    def train(self, authors_data, epochs=3,batch_size=16):
 
         self.label2id = {label: i for i, label in enumerate(authors_data.keys())}
         self.id2label = {i: label for label, i in self.label2id.items()}
@@ -57,3 +58,35 @@ class SequenceClassifier:
             outputs = self.model(**inputs)
         pred= outputs.logits.argmax().item()
         return self.id2label[pred]
+    
+    def evaluate_devset(self, dev_data, show_accuracy = False):
+        print("Results on dev set:")
+        for author, samples in dev_data.items():
+            all_accuracies = []
+            correct = 0
+            total = len(samples)
+            if total == 0: 
+                print(f"Skipping {author} (no test samples available)")
+                continue
+            for sample in samples:
+                if self.classify(sample) == author:
+                    correct+=1
+            accuracy = correct/total
+            if show_accuracy:
+                print(f"{author} \t {accuracy*100:.2f}% correct")
+            all_accuracies.append(accuracy)
+        return sum(all_accuracies)/len(all_accuracies) if all_accuracies else 0
+
+    def predict(self, test_data,save_predictions = True):
+        predictions = []
+        for sample in test_data:
+            predictions.append(self.classify(sample))
+        if save_predictions:
+            self.__save_predictions(predictions)
+        return predictions
+
+    
+    def __save_predictions(self,predictions, output_file = "predictions.txt"):
+        with open(output_file, "w") as f:
+            for pred in predictions:
+                f.write(f"{pred}\n")
